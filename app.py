0207c37b9c193a8b9e3c752e9e636bf58705b36c
ArtcalO <niyca14@gmail.com>
2024-01-18 11:47:12 +0200

    server starting correctly

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 from wsgiref.simple_server import make_server
 import falcon
 
-class CRUD(object):
+class CRUD:
 
-	def get():
+	def on_get(self,req, resp):
+		response.status = falcon.HTTP_200
+		resp.content_type = falcon.MEDIA_TEXT  # Default is JSON, so override
+		resp.text = (
+			'\nTwo things awe me most, the starry sky '
+			'above me and the moral law within me.\n'
+			'\n'
+			'	~ Immanuel Kant\n\n'
+		)
 
+api = falcon.App()
+crud = CRUD()
+api.add_route("/", crud)
 
-if __name__=="main":
-	api = falcon.API()
-	
-	api.add_route("/", CRUD())
+if __name__ == '__main__':
+    with make_server('', 8000, api) as httpd:
+        print('Serving on port 8000...')
 
-	with make_server("",8080) as api:
-		api.serve_forever()
+        # Serve until process is killed
+        httpd.serve_forever()
